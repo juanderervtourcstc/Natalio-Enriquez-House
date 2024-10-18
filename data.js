86531f040fc3316d6844c340d095e063cbf15030
Juanderer Virtual tour <juanderervtour@gmail.com>
2024-10-18 09:45:34 +0800

    first commit

---
+++ data.js
@@ -0,0 +1,94 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-front-view-",
+      "name": "Front View ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "yaw": 0.06466782394465653,
+        "pitch": -0.2869745598212319,
+        "fov": 1.1878338128377688
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8592479942102571,
+          "pitch": 0.12968749384724987,
+          "rotation": 0,
+          "target": "1-front-gate"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.367032940010926,
+          "pitch": 0.0013505024120838982,
+          "title": "<p class=\"MsoNormal\" style=\"text-align:justify\"><span style=\"font-size:14.0pt;\nline-height:107%;font-family:&quot;Times New Roman&quot;,serif\">Natalio Enriquez House<o:p></o:p></span></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size:14.0pt;line-height:107%;font-family:\n&quot;Times New Roman&quot;,serif\">Due to the current state of the Natalio Enriquez\nHouse, the researchers cannot enter and can only capture the outside view of\nthe house.<o:p></o:p></span></p>"
+        }
+      ]
+    },
+    {
+      "id": "1-front-gate",
+      "name": "Front Gate",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1767.5,
+      "initialViewParameters": {
+        "yaw": -0.01793146491775488,
+        "pitch": -0.297678930929413,
+        "fov": 1.3468096107558716
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4664574714099015,
+          "pitch": 0.07970795572968825,
+          "rotation": 0,
+          "target": "0-front-view-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Natalio Enriquez House",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
